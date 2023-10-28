23f588c8e60d8c95f7798f14b55f8f9d7f936ceb
dayngerous <dayneguy1@outlook.com>
2023-10-28 11:36:38 -0400

    View - Index Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import { IndexPage } from "./pages";
-import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
     <div className="App">
-      <ProjectPage />
+      <IndexPage />
     </div>
   );
 }
